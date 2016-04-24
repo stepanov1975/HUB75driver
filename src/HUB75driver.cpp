a8c34a16811ef220f833e25ab26fa180cf7df09d
Alex Stepanov <stepanov1975@gmail.com>
2016-04-24 15:59:43 +0300

    changed to panels like ADF

--- src/HUB75driver.cpp
+++ src/HUB75driver.cpp
@@ -4,6 +4,7 @@
 
 #include "HUB75driver.h"
 
+
 static HUB75driver *activePanel = NULL;
 
 HUB75driver::HUB75driver()
@@ -62,9 +63,14 @@ void HUB75driver::drive()
 	Binary coded modulation
 	Will need to execute cycles 0,1,3,7
 	*/
+
+	//*** Debug ***
+	//long int time_start = micros();
+	//*****
+
 	int addr, addr_pre,i;
-	uint8_t data_out;
-	uint8_t* img = matrixbuff[display_buffer_index];
+	uint8_t data_out,offswt=0;
+	uint8_t* img = matrixbuff[display_buffer_index],ptr;
 
 	switch (pwm_count) {
 	case 0:
@@ -72,34 +78,37 @@ void HUB75driver::drive()
 		addr_pre = line * 32 * 3;
 		break;
 	case 1:
-		addr_pre = line * 32 * 3 + 1;
+		addr_pre = line * 32 * 3 + 32;
 		break;
 	case 3:
-		addr_pre = line * 32 * 3 + 2;
+		addr_pre = line * 32 * 3 + 64;
 		break;
 	}
-	//*** Debug ***
-	long int time_start = micros();
-	//*****
+	
 	switch (pwm_count) {
 	case 0:
 	case 1:
 	case 3:
-		for (i = 0; i < 32*3; i+=3) {
-			addr = addr_pre + i;
-			PORTD = img[addr];
+		for (i = 0; i < 32 ; i++) {
+			//addr = addr_pre + i;
+			//ptr = img[addr_pre + i];
+			PORTD = img[addr_pre + i];
+
 			//PORTD = img[addr] & B11111100;
-			PORTB = PINB | B00000001;//clock high
-			PORTB = PINB & B11111110;//clock low
+			//PORTB = PINB | B00000001;//clock high
+			//PORTB = PINB & B11111110;//clock low
+			{__asm__ volatile ("SBI 5 , 0\n\t" "CBI 5 , 0\n\t"); }//toggle clock high,low
+			
 		}
 		break;
 	case 7:
-		for (i = 0; i < 32 * 3; i += 3) {
+		for (i = 0; i < 32; i++) {
 			addr = addr_pre + i;
-			data_out = ((img[addr] & B00000011) << 6) + ((img[addr + 1] & B00000011) << 4) + ((img[addr + 2] & B00000011) << 2);
+			data_out = ((img[addr] & B00000011) << 6) + ((img[addr + 32] & B00000011) << 4) + ((img[addr + 64] & B00000011) << 2);
 			PORTD = data_out;//PORTD = data_out & B11111100;
-			PORTB = PINB | B00000001;//clock high
-			PORTB = PINB & B11111110;//clock low
+			//PORTB = PINB | B00000001;//clock high
+			//PORTB = PINB & B11111110;//clock low
+			{__asm__ volatile ("SBI 5 , 0\n\t" "CBI 5 , 0\n\t"); }//toggle clock high,low
 		}
 		break;
 	}
@@ -110,11 +119,23 @@ void HUB75driver::drive()
 		PORTC = PINC | B00001000;//LAT high
 		PORTC = PINC & B11110111;//LAT low
 		PORTB = PINB & B11111101;//OE low
+		/*
+		{__asm__ volatile (
+			"SBI 5 , 1\n\t" //OE high PORTB
+			"SBI 8 , 3\n\t" //LAT high PORTC
+			"IN r16 , 6\n\t"//Read from PINC
+			"ANDI r16 , 0b11111000\n\t"
+			"LD r0, %a[line]\n\t"
+			"ADD r0, r16\n\t"
+			"OUT 8, r0\n\t"
+			"CBI 8 , 3\n\t" //LAT low
+			"CBI 5 , 1\n\t" //OE low
+		:
+		: [line]  "e" ((uint8_t *)line)
+		:"r16","r0");
+		}/**/
 	}
-	//*** Debug ***
-	long elapsed = micros() - time_start;
-	Serial.println(elapsed);
-	//********************
+	
 	if (pwm_count < 16) {
 		pwm_count++;
 	}
@@ -136,33 +157,48 @@ void HUB75driver::drive()
 			}
 		}
 	}
+	//*** Debug ***
+	//long elapsed = micros() - time_start;
+	//Serial.println(elapsed);
+	//********************
 }
 
 void HUB75driver::draw_point(unsigned char x, unsigned char y, unsigned char r, unsigned char g, unsigned char b)
 {
-	int addr_s, addr_s1, addr_s2;
-	uint8_t* img = matrixbuff[draw_buffer_index];
-	if (x < 0 || y < 0 || x>31 || y>15) return;
-	if (y < 8) {
-		addr_s = (y * 32 + x) * 3;
-		addr_s1 = addr_s + 1;
-		addr_s2 = addr_s + 2;
-		img[addr_s] = ((b&bit0) << 4) + ((g&bit0) << 3) + ((r&bit0) << 2) + (img[addr_s] & B11100011);
-		img[addr_s1] = ((b&bit1) << 3) + ((g&bit1) << 2) + ((r&bit1) << 1) + (img[addr_s] & B11100011);
-		img[addr_s2] = ((b&bit2) << 2) + ((g&bit2) << 1) + (r&bit2) + (img[addr_s] & B11100011);
-		img[addr_s1] = ((b&bit3) >> 3) + (img[addr_s1] & B11111110);
-		img[addr_s2] = ((g&bit3) >> 2) + ((r&bit3) >> 3) + (img[addr_s2] & B11111100);
+	/*
+	Using 4 bits per color,3 bytes packed like this
+	3 byte per 2 pixels
+	32*3 bytes per line
+	First 32 bytes is bit 0 in format B2,G2,R2,B1,G1,R1,x,x
+	Second 32 bytes bit 1
+	Third 32 bytes bit 2
+	Bit4 packed in lass two bits of each byte so for column n
+	n*3 B2,G2
+	n*3+1 R2,B1
+	n*3+2 G1,R1
 
+	*/
+	int addr_0, addr_1, addr_2;
+	int base = (y%8) * 32 * 3;
+	uint8_t* img = matrixbuff[draw_buffer_index];
+	addr_0 = base + x;
+	addr_1 = base + 32 + x;
+	addr_2 = base + 64 + x;
+
+	if (y < 8) {
+		//Set bits 5,4,3 - B1,G1,R1
+		img[addr_0] = ((b&bit0) << 4) + ((g&bit0) << 3) + ((r&bit0) << 2) + (img[addr_0] & B11100011);
+		img[addr_1] = ((b&bit1) << 3) + ((g&bit1) << 2) + ((r&bit1) << 1) + (img[addr_1] & B11100011);
+		img[addr_2] = ((b&bit2) << 2) + ((g&bit2) << 1) + (r&bit2) + (img[addr_2] & B11100011);
+		img[addr_1] = ((b&bit3) >> 3) + (img[addr_1] & B11111110);
+		img[addr_2] = ((g&bit3) >> 2) + ((r&bit3) >> 3) + (img[addr_2] & B11111100);
 	}
 	else {
-		addr_s = ((y - 8) * 32 + x) * 3;
-		addr_s1 = addr_s + 1;
-		addr_s2 = addr_s + 2;
-		img[addr_s] = ((b&bit0) << 7) + ((g&bit0) << 6) + ((r&bit0) << 5) + (img[addr_s] & B00011111);
-		img[addr_s1] = ((b&bit1) << 6) + ((g&bit1) << 5) + ((r&bit1) << 4) + (img[addr_s1] & B00011111);
-		img[addr_s2] = ((b&bit2) << 5) + ((g&bit2) << 4) + ((r&bit2) << 3) + (img[addr_s2] & B00011111);
-		img[addr_s] = ((b&bit3) >> 2) + ((g&bit3) >> 3) + (img[addr_s] & B11111100);
-		img[addr_s1] = ((r&bit3) >> 2) + (img[addr_s1] & B11111101);/**/
+		img[addr_0] = ((b&bit0) << 7) + ((g&bit0) << 6) + ((r&bit0) << 5) + (img[addr_0] & B00011111);
+		img[addr_1] = ((b&bit1) << 6) + ((g&bit1) << 5) + ((r&bit1) << 4) + (img[addr_1] & B00011111);
+		img[addr_2] = ((b&bit2) << 5) + ((g&bit2) << 4) + ((r&bit2) << 3) + (img[addr_2] & B00011111);
+		img[addr_0] = ((b&bit3) >> 2) + ((g&bit3) >> 3) + (img[addr_0] & B11111100);
+		img[addr_1] = ((r&bit3) >> 2) + (img[addr_1] & B11111101);
 	}
 }
 
